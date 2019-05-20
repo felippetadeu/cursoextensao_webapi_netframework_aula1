e8d64ffc9319fcc046b1ca0fa840dadad3c23dd3
Felippe Tadeu Affonso Santos da Costa <felippe_tadeu@hotmail.com>
2019-05-20 09:59:52 -0300

    Retirada de arquivos referente ao MVC

--- Aula1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Aula1.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Aula1/Provider/ProviderDeTokensDeAcesso.cs
+++ Aula1/Provider/ProviderDeTokensDeAcesso.cs
@@ -15,24 +15,30 @@ namespace Aula1.Provider
         public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
         {
             context.Validated();
-            return null;
+            return Task.FromResult<object>(null);
         }
 
         public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
+            //Aqui é feita a verificação do usuário
             if (UsuarioSeguranca.Login(context.UserName, context.Password))
             {
+                //Instanciado o objeto de identidade de direitos
                 var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                //Adicionado o direito sub com valor do nome do usuário
                 identity.AddClaim(new Claim("sub", context.UserName));
+                //Adicionado o direto role com valor user
                 identity.AddClaim(new Claim("role", "user"));
+
                 context.Validated(identity);
-                return null;
+                return Task.FromResult<object>(identity);
             }
             else
             {
                 context.SetError("acesso inválido", "As credenciais do usuário não conferem....");
-                return null;
             }
+
+            return Task.FromResult<object>(null);
         }
     }
 }
